536dd9301a8ff55852c9d55c66bf8f47199caa11
Thomas <thomas.g.satnav@btinternet.com>
2022-12-01 23:58:40 +0000

    Javascript and Design Rework

--- js/script.js
+++ js/script.js
@@ -7,157 +7,229 @@ po.src = 'https://apis.google.com/js/plusone.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 })();*/
 
-
-
-
 var score = 0;
 var step = 0;
 var questions = [];
 var result = 0;
-var numberqs = 0
+var numberqs = 0;
 var answers = [];
 var talk = [];
 var responsesYes = [];
 var responsesNo = [];
 var teams = [];
 
-questions['ita'] = [
-	'',
-];
+class Question {
+  constructor(question, yesAns, noAns, questionDesc) {
+    this.question = question;
+    this.yesAns = yesAns;
+    this.noAns = noAns;
+    this.questionDesc = questionDesc;
+  }
 
-questions['eng'] = [
-	'',
-	'Reason for demand?',
-	'How would you prefer this delivered?',
-	'Who can carry out the work and who decides?',
-    'What level of control is required?<p><br>Who Decides what work is done?<br>Who Decides How the work is delivered?<br>Who has control over changing what work is done?</p>',
-	'Is supervision required?<p><br>Is work under constant review or does the eview take place when the agreed output is delivered?<br>Is the indicidual self reliant or are they being mentored / Developed?</p>',
-	'Do they need direction?<p><br>Is specific instruction provided on how the work is delivered or does the individual determine how to deliver the outcome?<br>Is client coordingating how the work is being done as it is undertaken?</p>',
-	'How will they be paid?',
-];
+  formatAnswer(answerList) {
+    if (answerList == null) {
+      return "";
+    }
 
-responsesYes['yesAnswers'] = [
-	'',
-	'A clear project with a defined outcome\nDefined milestones to be met by Rullion',
-	'Are you making a change that effects the sales process',
-	'Does your change require form changes',
-    'Does you change have anything to do with web leads',
-	'',
-	'',
-	'',
-	
-];
+    let arrayOfAnswers = answerList;
+    let formattedAnswers = "";
+    arrayOfAnswers.forEach((answer, i) => {
+      if (i != arrayOfAnswers.length - 1) {
+        formattedAnswers = formattedAnswers + answer + "<br><br>";
+      } else {
+        formattedAnswers = formattedAnswers + answer;
+      }
+    });
+    return formattedAnswers;
+  }
 
-responsesNo['noAnswers'] = [
-	'',
-	'Reason for demand?',
-	'Are you making a change that effects the sales process',
-	'Does your change require form changes',
-    'Does you change have anything to do with web leads',
-	'',
-	'',
-	'',
-	
-];
+  formatDesc(descriptionList) {
+    if (descriptionList == null) {
+      return "";
+    }
 
-answersTitle = [
-    'Rullion Change Delivery',
-	'Recruitment',
-];
+    let arrayOfDescriptions = descriptionList;
+    let formattedDescriptions = "";
+    arrayOfDescriptions.forEach((description, i) => {
+      formattedDescriptions =
+        formattedDescriptions + "<li>" + description + "</li>";
+    });
+    return formattedDescriptions;
+  }
 
-answersText = [
-    'Rullion Change Delivery',
-	'Recruitment',
-];
+  getYes() {
+    return this.formatAnswer(this.yesAns);
+  }
+
+  getNo() {
+    return this.formatAnswer(this.noAns);
+  }
+
+  getDesc() {
+    return this.formatDesc(this.questionDesc);
+  }
+}
+
+let q1 = new Question(
+  "Reason for demand?",
+  [
+    "A clear project with a defined outcome",
+    "Defined milestones to be met by Rullion",
+    "Payment on completion of milestones",
+    "A defined work package where Rullion are accountable for delivery",
+  ],
+  [
+    "Provision of an individual with a specific skillset",
+    "Covering maternity leave/sickness absence",
+    "Additional resource required, e.g., to work on a project where Client is accountable for delivery",
+  ]
+);
+
+let q2 = new Question(
+  "How would you prefer this delivered?",
+  [
+    "No preference - can be delivered from anywhere unless physical constraints to the work (e.g., physical installation, etc.)",
+    "Rullion arrange work, hours and location (in agreement with Client)",
+    "Rullion responsible for selection of the team with a single ‘Client Introduction’ meeting included in the process",
+  ],
+  [
+    "Decide how and when work is done. For example:",
+    "In a fixed location",
+    "Pattern of work defined by Client (e.g., number of hours, when worked)",
+    "Client controls the individual task management",
+  ]
+);
+
+let q3 = new Question(
+  "Who can carry out the work and who decides",
+  ["Rullion can provide and pay for a suitable substitution if required"],
+  ["Client decides which individual will carry out the work"]
+);
+
+let q4 = new Question(
+  "What level of control is required?",
+  [
+    "Control is provided by Rullion with little effort exerted by Client",
+    "Client instruct Rullion not individuals directly",
+  ],
+  [
+    "A significant amount of control is exerted by Client",
+    "Direct reporting line into Client management",
+  ],
+  [
+    "Who decides what work is done?",
+    "Who decides HOW the work is delivered?",
+    "Who has control over changing what work is done?",
+  ]
+);
+
+let q5 = new Question(
+  "Is supervision required?",
+  [
+    "Rullion Delivery Manager provides supervision",
+    "Milestones agreed – Client approve / decline against the output",
+  ],
+  [
+    "Client Management provides all supervision",
+    "Client oversee all aspects of individual's work",
+  ],
+  [
+    "Is work under constant review or does the review take place when the agreed output is delivered?",
+    "Is the individual self reliant or are they being mentored / developed?",
+  ]
+);
+
+let q6 = new Question(
+  "Do they need direction?",
+  [
+    "Rullion agrees ways of working with Client",
+    "Regular service feedback loop with Rullion",
+  ],
+  [
+    "Direction and instruction given directly by Client to individual as the work is being undertaken",
+  ],
+  [
+    "Is specific instruction provided on how the work is delivered or does the individual determine how to deliver the outcome?",
+    "Is Client coordinating how the work is being done as it is undertaken?",
+  ]
+);
+
+let q7 = new Question(
+  "How will they be paid?",
+  [
+    "Fixed Price Outcome Based",
+    "Payment made once milestone completed and output accepted",
+  ],
+  [
+    "Time & Materials",
+    "Daily rate, paid for number of days work completed regardless of output",
+  ]
+);
+
+let questionList = [q1, q2, q3, q4, q5, q6, q7];
+
+let a1 = new Question("Rullion Change Delivery", null, null, [
+  "Specialists",
+  "Teams",
+  "360",
+]);
+
+let a2 = new Question("Rullion Managed Teams", null, null, [
+  "Contingent Labour",
+  "FTC Resource",
+]);
 
 function getLang() {
-	var userLang = navigator.language || navigator.userLanguage;
-	var lang = userLang.split('-');
+  var userLang = navigator.language || navigator.userLanguage;
+  var lang = userLang.split("-");
 
-	if (lang[0] == 'it')
-		return 'ita';
-	else
-		return 'eng';
+  if (lang[0] == "it") return "ita";
+  else return "eng";
 }
 
 function answer(res) {
-	if (res == 'yes') {
-        score = score + 1;
-        next();
-		//talk.push(answers[step]);
-		//$('#buttons-box').hide();
-		//$('#drop-msg').show();
-		//$('#share-button').show();
-       // talk.push(answers[step])
-
-		//ga('send', 'event', 'test', 'completed', 'no');
-	}
-	else
-		next();
+  if (res == "yes") {
+    score = score + 1;
+    next();
+  } else next();
 }
 
 function next() {
-    numberqs = questions.eng.length - 1
-	if (step == numberqs) {
-		result = 1;
-        
-		$('#result').show();
-        //teams = talk.join('</br>');
-		if(score > 3){
-			document.getElementById("resultTitle").innerHTML = answersTitle[0];
-			document.getElementById("resultText").innerHTML = answersText[0];
-		}
-		else{
-			document.getElementById("resultTitle").innerHTML = answersTitle[1];
-			document.getElementById("resultText").innerHTML = answersText[1];
-		}
-		
-        
-        //$('#step').hide();
-		document.getElementById("title").innerHTML = "Your recommended product is";
-		$('#btnRestart').show();
-		$('#title').show();
-        $('#question').hide();
-		$('#ansYes').hide();
-		$('#ansNo').hide();
-		//$('#share-button').show();
+  numberqs = questionList.length - 1;
+  if (step == numberqs) {
+    result = 1;
 
-		//ga('send', 'event', 'test', 'completed', 'yes');
-	}
-	else {
-		step++;
-        
-		//$('#step').html(step  * numberqs / numberqs);
-		document.getElementById("ansYes").innerText = responsesYes['yesAnswers'][step];
-		document.getElementById("ansNo").innerText = responsesNo['noAnswers'][step];
-		$('#question').html(questions['eng'][step]);
-	}
-}
+    $("#result").show();
+    if (score > 3) {
+      document.getElementById("resultTitle").innerHTML = a1.question;
+      document.getElementById("resultText").innerHTML = a1.getDesc();
+    } else {
+      document.getElementById("resultTitle").innerHTML = a2.question;
+      document.getElementById("resultText").innerHTML = a2.getDesc();
+    }
 
-/*function nextq() {
-    numberqs = questions.eng.length - 1
-	if (step == numberqs) {
-		result = 1;
-
-		$('#buttons-box').hide();
-		$('#ask-msg').show();
-		$('#share-button').show();
-
-		//ga('send', 'event', 'test', 'completed', 'yes');
-	}
-    else  {
+    document.getElementById("title").innerHTML = "Your recommended product is";
+    $("#btnRestart").show();
+    $("#title").show();
+    $("#question").hide();
+    $("#questionDesc").hide();
+    $("#ansYes").hide();
+    $("#ansNo").hide();
+  } else {
     step++;
 
-		$('#step').html(step  * numberqs / numberqs);
-		$('#question').html(questions[getLang()][step] + '?' + answers[step]);
-        talk.push(answers[step -1]);
-    }
-}*/
+    $("#question").html(questionList[step].question);
+    document.getElementById("questionDesc").innerHTML =
+      questionList[step].getDesc();
+    document.getElementById("ansYes").innerHTML = questionList[step].getYes();
+    document.getElementById("ansNo").innerHTML = questionList[step].getNo();
+  }
+}
 
 function start() {
-	$('#ansYes').show();
-	$('#ansNo').show();
-	$('#title').hide();
-	$('#btnStart').hide();
-	next();
+  $("#ansYes").show();
+  $("#ansNo").show();
+  $("#title").hide();
+  $("#btnStart").hide();
+  next();
 }
